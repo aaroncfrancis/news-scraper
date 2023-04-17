29fd8fc713d78a0772c9aaf8d129f9dcae845781
Aaron Francis <aaronfrancis99@gmail.com>
2023-04-16 21:48:35 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import requests
+import mysql.connector
+from bs4 import BeautifulSoup
+import datetime
+
+# establish mySQL connection 
+def conntectMySQl(): 
+        connection = mysql.connector.connect(
+            host='localhost',
+            user = 'aaroncfrancis',
+            password = 'samplepassword',
+            database = 'news'
+        )
+        if connection.is_connected():
+            print('Successful connection')
+        return connection
+
+def createNewsTable(connection):
+      cursor = connection.cursor()
+      cursor.execute()
+      connection.commit()
+      print('news table created')
+
+# Add news data into the table
+def insertNewsData(connection, headline, summary, url, publication_date, citation): 
+      cursor = connection.cursor()
+      query = "Insert into news (headline, summary, url, publication_date, citation)"
+      data = (headline, summary, url, publication_date, citation)
+      cursor.execute(query, data)
+      connection.commit()
+      print('data successfully inserted into the table')
+
+# Scraper
+def scrapeAndStore():
+      url = 'https://www.bbc.com/news'
+      response = requests.get(url)
+      soup = BeautifulSoup(response.text, 'html.parser')
+      
+      headlines =  [headline.text for headline in soup.find_all('h2')]
+      summaries = [summary.text for summary in soup.find_all('p')]
+      urls = [url["href"] for url in soup.find_all]
+      publication_dates = [date.text for date in soup.find_all('span', class_='date')]
+      citations = [citation.text for citation in soup.find_all('span', class_='citation')]
+      print(type(headlines))
+    
+
+
+    
+
+        
